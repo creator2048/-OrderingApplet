7376abc3c7adf764db0e4a476d1a562953d89d37
Test <40850853+creator2048@users.noreply.github.com>
2019-11-25 10:02:51 +0800

    Add files via upload

---
+++ pages/list/list.js
@@ -0,0 +1,41 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList: [
+      {
+        goods_id: 1,
+        goods_title: '商品标题1',
+        goods_img: '../../images/01.jpg',
+        goods_xiaoliang: '0',
+        goods_price: '60'
+      }, {
+        goods_id: 1,
+        goods_title: '商品标题2',
+        goods_img: '../../images/01.jpg',
+        goods_xiaoliang: '0',
+        goods_price: '70'
+      }, {
+        goods_id: 1,
+        goods_title: '商品标题3',
+        goods_img: '../../images/01.jpg',
+        goods_xiaoliang: '0',
+        goods_price: '80'
+      }, {
+        goods_id: 1,
+        goods_title: '商品标题4',
+        goods_img: '../../images/01.jpg',
+        goods_xiaoliang: '0',
+        goods_price: '90'
+      }, {
+        goods_id: 1,
+        goods_title: '商品标题5',
+        goods_img: '../../images/01.jpg',
+        goods_xiaoliang: '0',
+        goods_price: '110'
+      }
+    ],
+  }
+})
